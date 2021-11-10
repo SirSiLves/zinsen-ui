971e40ef9ccc2317234c2a9bb81bf2f153833bc0
Yves Ruosch <yves@ruosch.me>
2021-11-10 21:47:30 +0100

    error view improved

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ZinsQuery } from '../../../openapi';
 import { Observable } from 'rxjs';
 import { ZinsenService } from '../../services/zinsen.service';
-import { map, shareReplay, tap } from 'rxjs/operators';
+import { catchError, map, shareReplay, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-dashboard',
@@ -12,13 +12,20 @@ import { map, shareReplay, tap } from 'rxjs/operators';
 export class DashboardComponent implements OnInit {
 
   zinsen$!: Observable<ZinsQuery[]>;
+  error: Error | undefined = undefined;
 
   constructor(
     private zinsenService: ZinsenService
-  ) { }
+  ) {
+  }
 
   ngOnInit(): void {
-    this.zinsen$ = this.zinsenService.getZinsen().pipe(shareReplay());
+    this.zinsen$ = this.zinsenService.getZinsen().pipe(
+      shareReplay(),
+      catchError(error => {
+        this.error = error;
+        throw error;
+      }));
   }
 
 }
