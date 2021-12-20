c83ac26b36ffb5e6aebac0bb4c3091e5796eee37
Yves Ruosch <yves@ruosch.me>
2021-12-20 23:25:53 +0100

    Added Applications Insights

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   backendUrl: 'https://zinsen-api.azurewebsites.net',
   appInsights: {
-    roleName: 'zinsen-ui.azurewebsites.net',
+    roleName: 'TEST zinsen-ui',
     instrumentationKey: '37402c98-17e2-4f2c-a6ac-0355e698ff34'
   }
 };
