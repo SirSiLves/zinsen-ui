eff1519a3bbbeb3d0b60454bf2c796b10080a5e8
Yves Ruosch <yves@ruosch.me>
2021-12-22 12:03:38 +0100

    UI Updated

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import versions from '../../../_versions';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  version?: string
+
   constructor() { }
 
   ngOnInit(): void {
+    this.version = versions.versionLong;
   }
 
 }
